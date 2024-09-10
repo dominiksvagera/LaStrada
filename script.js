353dda2d9ba0fd30cbe19a6b2eb07cb337839fd8
Dominik <dominik.svagera@gmail.com>
2024-09-10 20:30:48 +0200

    js init

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('Hello, world!');
+
+let nazev = 'Tome';
+
+var vek = 25;
+
+let pravda = true;
+
+console.log(nazev);
+console.log(vek);
+console.log(pravda);
+
+let vek2 = 100;
+
+console.log(vek2);
+console.log(vek2+vek);
+
+var nazev2 = 'Ahoj';
+
+console.log(nazev2 + " " + nazev);
+
+const konstanta = 10;
+
+console.log(konstanta);
+
+var nazev2 = 'Hello';
+
+console.log(nazev2);
+
